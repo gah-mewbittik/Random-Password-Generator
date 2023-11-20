6d3fc0b0a2aa827c8521c7e046fde875d9e3552a
Andrew Biron <andrewbiron@andrews-air.home>
2023-11-20 10:14:04 -0500

    Updated code

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -12,3 +12,155 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+  
+///start project
+//Arrays of lowercaseArr, numArr, and characters
+var lowercaseArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x','y', 'z'];
+var uppercaseArr = ['A', 'B', 'C', 'D','E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var numArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var specialCharacterArr = ['!', '@', '#', "$",'%', '^', '&', '*', '(', ')', "-", '_', '=', '+', '[', ']', '{', '}', ';', ':', "'",'"', '|', ',', '.', '<','>','/', '?'];
+
+// Array used to combine(concat) above arrays
+var arrSetup= [];
+
+// start of generatePassword //
+function generatePassword(){
+  //initial prompt question - Password size request
+  let numberOfChar = Number(prompt("Type the number of characters you want your password to be: "));
+  
+  // empty string, but will be the random generate password
+  var randPass = '';
+
+
+/// # of character tests  min = 8, max =128, and empty string 
+ if(numberOfChar == "") {
+  alert('ERROR. Enter a minimum of and a maximum of 128 characters. ONLY enter number.');
+ }else if(numberOfChar < 8){
+  alert('ERROR. Enter a minimum of and a maximum of 128 characters. ONLY enter number.');
+ }else if(numberOfChar > 128){
+  alert('ERROR. Enter a minimum of and a maximum of 128 characters. ONLY enter number.');
+
+}else{
+  // object that contains the questions need for this project 
+  var passConfirms = {
+    lowerQuote: confirm("Would you like to use lowercase letters?"),
+    upperQuote: confirm("Would you like to use uppercase letters?"),
+    numberQuote: confirm("Would you like to use numbers?"),
+    specialQuote: confirm("Would you like to use special characters?")
+  }
+
+  ////////Switch starts
+  switch(true){
+
+    // lowercase question options
+    case passConfirms.lowerQuote && passConfirms.upperQuote && passConfirms.numberQuote && passConfirms.specialQuote:
+      arrSetup = lowercaseArr.concat(uppercaseArr, numArr, specialCharacterArr);
+      for(var i = 0; i < numberOfChar; i++){      
+        randPass += arrSetup[(Math.floor(Math.random() * arrSetup.length))];
+      }
+      return randPass;
+    
+    case passConfirms.lowerQuote  && passConfirms.upperQuote && passConfirms.numberQuote:
+      arrSetup = lowercaseArr.concat(uppercaseArr, numArr);
+      for(var i = 0; i < numberOfChar; i++){      
+        randPass += arrSetup[(Math.floor(Math.random() * arrSetup.length))];
+    }
+    return randPass;
+
+    case passConfirms.lowerQuote && passConfirms.numberQuote && passConfirms.specialQuote:
+      arrSetup = lowercaseArr.concat( numArr, specialCharacterArr);
+      for(var i = 0; i < numberOfChar; i++){      
+        randPass += arrSetup[(Math.floor(Math.random() * arrSetup.length))];
+      }
+      return randPass;
+
+      case passConfirms.lowerQuote && passConfirms.specialQuote:
+        arrSetup = lowercaseArr.concat( specialCharacterArr);
+        for(var i = 0; i < numberOfChar; i++){      
+          randPass += arrSetup[(Math.floor(Math.random() * arrSetup.length))];
+        }
+        return randPass;
+
+    case passConfirms.lowerQuote && upperQuote:
+      arrSetup = lowercaseArr.concat(uppercaseArr);
+      for(var i = 0; i < numberOfChar; i++){      
+        randPass += arrSetup[(Math.floor(Math.random() * arrSetup.length))];
+      }
+      return randPass;
+
+    case passConfirms.lowerQuote:
+      arrSetup = lowercaseArr;
+      for(var i = 0; i < numberOfChar; i++){      
+          randPass += arrSetup[(Math.floor(Math.random() * arrSetup.length))];
+      }
+      return randPass;
+
+    /////uppercase question options
+    
+    case passConfirms.upperQuote && passConfirms.numberQuote && passConfirms.specialQuote:
+      arrSetup = uppercaseArr.concat(numArr, specialCharacterArr);
+      for(var i = 0; i < numberOfChar; i++){      
+        randPass += arrSetup[(Math.floor(Math.random() * arrSetup.length))];
+      }
+      return randPass;
+
+    case passConfirms.upperQuote && passConfirms.numberQuote:
+      arrSetup = uppercaseArr.concat(numArr);
+      for(var i = 0; i < numberOfChar; i++){      
+        randPass += arrSetup[(Math.floor(Math.random() * arrSetup.length))];
+      }
+      return randPass;
+
+      case passConfirms.upperQuote && passConfirms.specialQuote:
+        arrSetup = uppercaseArr.concat(specialCharacterArr);
+        for(var i = 0; i < numberOfChar; i++){      
+          randPass += arrSetup[(Math.floor(Math.random() * arrSetup.length))];
+        }
+        return randPass;
+
+    case passConfirms.upperQuote:
+      arrSetup = uppercaseArr;
+      for(var i = 0; i < numberOfChar; i++){      
+        randPass += arrSetup[(Math.floor(Math.random() * arrSetup.length))];
+      }
+      return randPass;
+ 
+///// number Question options
+
+    case passConfirms.numberQuote && passConfirms.specialQuote:
+      arrSetup = numArr.concat(specialCharacterArr);
+      for(var i = 0; i < numberOfChar; i++){      
+        randPass += arrSetup[(Math.floor(Math.random() * arrSetup.length))];
+      }
+      return randPass;
+
+    case passConfirms.numberQuote:
+      arrSetup = numArr;
+      for(var i = 0; i < numberOfChar; i++){      
+        randPass += arrSetup[(Math.floor(Math.random() * arrSetup.length))];
+      }
+      return randPass;
+
+    // special question option
+
+    case passConfirms.specialQuote:
+      arrSetup = specialCharacterArr;
+      for(var i = 0; i < numberOfChar; i++){      
+        randPass += arrSetup[(Math.floor(Math.random() * arrSetup.length))];
+      }
+      return randPass;
+
+    default:
+      alert('ERROR. Enter a minimum of and a maximum of 128 characters. ONLY enter number.');
+        
+  }////  end of switch
+
+ }
+ 
+
+}////  end of GeneratePassword function
+
+
+
+
